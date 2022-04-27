bad8e7e92b5bdd9f7341057c24b522863a3c74a2
Aleksey Boldyrev <aleksey.boldyrev@gmail.com>
2022-04-27 13:20:44 +0300

    Step 3

---
+++ app/controllers/web/bulletins_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Web
+  class BulletinsController < Web::ApplicationController
+    def index
+      @bulletins = Bulletin.order(created_at: :desc)
+    end
+
+    def new
+      @bulletin = Bulletin.new
+    end
+
+    def create
+      @bulletin = Bulletin.new(bulletin_params)
+      @bulletin.user = current_user
+
+      if @bulletin.save
+        redirect_to root_path, notice: t('bulletins.create.success')
+      else
+        render :new
+      end
+    end
+
+    def show; end
+
+    private
+
+    def bulletin_params
+      params.require(:bulletin).permit(:title, :description, :category_id, :image)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@
 
 Rails.application.routes.draw do
   scope module: :web do
-    root 'home#index'
+    root 'bulletins#index'
+
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 
     resources :sessions, only: :destroy
+
+    resources :bulletins, only: %i[index new create show]
   end
 end
